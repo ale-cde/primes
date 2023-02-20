8743bb896d91c286c070a0c13eab964aea74b26c
Alejandro <ale.arango42@gmail.com>
2023-02-20 19:50:29 +1000

    changed input to 10

--- fibonacci.py
+++ fibonacci.py
@@ -4,11 +4,12 @@ Created on Sat Sep 14 12:44:55 2019
 
 @author: alejandro
 """
-#fibonacci
-a=int(input())
+#%% fibonacci
+a=10
 l=[0,1]
 for i in range (2,a+1):
   b=l[i-1]+l[i-2]
   l.append(b)
 k=l[1:]
 print(k)
+# %%
